9678dece997337462430ee5047cff918cfb1bf46
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-04-01 20:22:45 +0300

    created barchart and installed redux as well as passport

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class AuthController extends Controller
-{
-    //
+class AuthController extends Controller{
+    
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            "fullname" => "required",
+            "email" => "required|email",
+            "password" => "required",
+            "confirmed_password" => "required|same:password", 
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                "status" => false,
+                "message" => "validation error",
+                "data" => $validator->errors()->all(),
+            ]);
+        };
+
+        return response()->json([
+            "status" => true,
+            "message" => "registration complete",
+        ]);
+
+    }
 }

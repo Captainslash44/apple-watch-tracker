9fb1a87b52eca37dc160db0259b7129dd7a0e742
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-04-01 21:07:05 +0300

    got access to csv data from upload

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import "./styles.css";
 
 const Header = ({ user, className }) => {
+  const [jsonData, setJsonData] = useState(null);
+
+  const convertCSVToJson = (csvData) => {
+    const lines = csvData.split("\n");
+
+    const headers = lines[0].split(",");
+    const result = [];
+
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {};
+      const currentLine = lines[i].split(",");
+
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentLine[j];
+      }
+
+      result.push(obj);
+    }
+    return result;
+  };
+
+  const handleCSVInputChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const csvData = e.target.result;
+
+      const jsonData = convertCSVToJson(csvData);
+      setJsonData(jsonData);
+    };
+
+    reader.readAsText(file);
+  };
+
   return (
     <header className={`flex space-between pl black-bg  ${className}`}>
       <h1 className="mont-font">Welcome {user}</h1>
       <div className="flex space-between">
-        <input type="file" name="file" accept=".csv" />
-        <Button text="Upload" />
+        <input
+          type="file"
+          name="file"
+          accept=".csv"
+          onChange={handleCSVInputChange}
+        />
+        <Button
+          text="Upload"
+          onClick={() => {
+            console.log(jsonData);
+          }}
+        />
       </div>
     </header>
   );
